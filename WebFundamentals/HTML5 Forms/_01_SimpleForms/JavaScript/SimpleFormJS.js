1fb860cac8bb3c16e526eaaf7424736d2f256673
GeorgeAngelov <geogreangelov93@gmail.com>
2014-06-16 03:05:42 +0200

    Adding First part of homework

---
+++ WebFundamentals/HTML5 Forms/_01_SimpleForms/JavaScript/SimpleFormJS.js
@@ -0,0 +1,11 @@
+﻿function checkPasswordMatch() {
+    var password = $("#pass").val();
+    var confirmPassword = $("#confPass").val();
+
+    if (password != confirmPassword) {
+        alert("Passwords do not match!");
+    }
+ $(document).ready(function () {
+   $("#confirmPass").keypress(checkPasswordMatch);
+});
+}
